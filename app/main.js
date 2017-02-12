dd7085821897b3cdc2ba37213037a03cc9cd9948
Andrew Caulfield <andrewjcaulfield@gmail.com>
2017-02-12 17:16:03 +0000

    Clean up linting errors inside of main.js

--- app/main.js
+++ app/main.js
@@ -4,19 +4,19 @@ const modal = document.getElementById('myModal');
 // Get the button that opens the modal
 const btn = document.getElementById('myBtn');
 
-const overlay = document.getElementById("myOverlay");
+const overlay = document.getElementById('myOverlay');
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
   modal.style.display = 'block';
-  overlay.style.display = "block";
+  overlay.style.display = 'block';
 };
 
 // When the user clicks anywhere outside of the modal, close it
 document.onclick = function(e) {
   if (e.target.id === 'myOverlay') {
     modal.style.display = 'none';
-     overlay.style.display = "none";
+    overlay.style.display = 'none';
   }
 };
 
